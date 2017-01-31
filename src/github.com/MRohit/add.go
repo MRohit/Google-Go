e7c446d6aaf005e9a3f9133d03b11ac295878888
Rohit <mourya.rohit32@gmail.com>
2017-01-31 12:53:22 +0530

    Playing with data types
    Understanding various data types of Go

---
+++ src/github.com/MRohit/add.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main(){
+	var a, b,c int;
+	b = 10
+	a=5
+	c=a+b
+	fmt.Print("Addition is:")
+	fmt.Println(c);
+	fmt.Printf("c is of type %T\n",c);
+	var x float64
+   x = 20.0
+   fmt.Println(x)
+   fmt.Printf("x is of type %T\n", x)
+   
+   y := 40.0
+   fmt.Println(y)
+   fmt.Printf("y is of type %T\n", y)
+   //10=20
+}
