ed2102f9dfbf9654ad954fb15ad9e830ed1deff1
Rohit <mourya.rohit32@gmail.com>
2017-02-02 18:53:22 +0530

    understanding switch case

---
+++ src/github.com/MRohit/switch.go
@@ -0,0 +1,35 @@
+package main
+import "fmt"
+
+func main(){
+	var marks int = 95
+	var grade string = "B"
+	switch marks{
+		case 95:
+			grade = "A+"
+		case 80:
+			grade = "A"
+		case 50,60,70:
+			grade = "B"
+		case 30:
+			grade = "C"
+	}
+	switch {
+		case grade == "A+":
+			fmt.Printf("Excellent\n")
+			
+		case grade == "A":
+			fmt.Printf("Very Good\n")
+			
+		case grade == "B":
+			fmt.Printf("You Passed\n")
+			
+		case grade == "C":
+			fmt.Printf("You Loser\n")
+		default:
+         fmt.Printf("Invalid grade\n" );
+			
+			
+	}
+	fmt.Printf("Your grade is  %s\n", grade );
+}
