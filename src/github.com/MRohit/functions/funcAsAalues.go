489f8ce96bfd4efd9c02a0592d38bc75f2443e1f
Rohit <mourya.rohit32@gmail.com>
2017-03-15 18:03:16 +0530

    Functions as Values

--- src/github.com/MRohit/functions/funcAsAalues.go
+++ src/github.com/MRohit/functions/funcAsAalues.go
@@ -8,6 +8,11 @@ func main(){
 	getSquareRoot := func(a float64) float64 {
 		return math.Sqrt(a)
 	}
+	getAddition := func(a,b float64) float64 {
+		return a+b
+	}
 	
 	fmt.Printf("\nSquare root of 169 is %f",getSquareRoot(169))
+	
+	fmt.Printf("\nAddition of 10 & 20 is %f",getAddition(10,20))
 }
