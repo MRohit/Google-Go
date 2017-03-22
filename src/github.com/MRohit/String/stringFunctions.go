ad54e4eb08ef83aaa296948bdb65f939d49d825c
Rohit <mourya.rohit32@gmail.com>
2017-03-22 16:25:54 +0530

    String Utility Functions

--- src/github.com/MRohit/String/stringFunctions.go
+++ src/github.com/MRohit/String/stringFunctions.go
@@ -14,4 +14,10 @@ func main(){
 	var str2 = []string {"This moment", "We Own it"}
 	
 	fmt.Printf(strings.Join(str2," "))
+	
+	// Compare two strings	
+	fmt.Printf("\n%d",strings.Compare(str,str2[0]))
+	
+	// Contains function
+	fmt.Printf("\n%t",strings.Contains(str,"moment"))
 }
