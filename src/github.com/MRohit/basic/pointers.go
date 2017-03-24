1cd04de35c770cd40c6e3fa8aa3f7173c53ab82e
Rohit <mourya.rohit32@gmail.com>
2017-03-24 18:05:24 +0530

    Double pointers

--- src/github.com/MRohit/basic/pointers.go
+++ src/github.com/MRohit/basic/pointers.go
@@ -14,4 +14,14 @@ func main() {
 	for i:=0;i<MAX;i++ {
 		fmt.Printf("\nValue of  a[%d] = %d",i,*ptr[i])
 	}
+	
+	var b int = 5
+	var bptr* int;
+	var	bptr2** int;
+	bptr = &b
+	bptr2 = &bptr 
+	
+	fmt.Printf("\nb Value:%d",b)
+	fmt.Printf("\nSingle Pointer bptr:%d",*bptr)
+	fmt.Printf("\nDouble Pointer bptr2:%d",**bptr2)
 }
