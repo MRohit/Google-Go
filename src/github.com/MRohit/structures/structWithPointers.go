8fccaf793986337114874c1be4c6fdc3a709277d
Rohit <mourya.rohit32@gmail.com>
2017-03-27 19:32:03 +0530

    Structure with pointers

---
+++ src/github.com/MRohit/structures/structWithPointers.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+		"fmt" 
+		)
+
+type language struct {
+	id int
+	name string
+	features string
+}
+
+func main () {
+	var lg1 language
+	var languagePtr *language
+	lg1.id = 1
+	lg1.name = "C"
+	lg1.features = "Procedural, case sensitive"
+	languagePtr = &lg1
+	
+	fmt.Printf("\nLanguage details:")
+	fmt.Printf("\nId\tName\tFeatures\n")
+	printStudeDetails(languagePtr);
+	
+}
+
+func printStudeDetails(lg1* language) {
+	fmt.Printf("\n%d\t%s\t%s",lg1.id,lg1.name,lg1.features)
+}
