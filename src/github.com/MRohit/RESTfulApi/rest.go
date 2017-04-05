dac155f6b7063c4ced8e756f4ed64b78b12d5693
Rohit <mourya.rohit32@gmail.com>
2017-04-05 18:39:25 +0530

    REST API update for JSON

--- src/github.com/MRohit/RESTfulApi/rest.go
+++ src/github.com/MRohit/RESTfulApi/rest.go
@@ -13,14 +13,17 @@ func main() {
     router.HandleFunc("/", Index)
     router.HandleFunc("/todos", TodoIndex)
     router.HandleFunc("/todos/{todoId}", TodoShow)
-
+	router.HandleFunc("/json/{todoId}", getJsonData)
     log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintln(w, "Welcome!")
 }
-
+func getJsonData(w http.ResponseWriter, r* http.Request) {
+	json := "{\"key\":\"12312312\",\"value\":\"Pass\"}"
+	
+}
 func TodoIndex(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintln(w, "Todo Index!")
 }
