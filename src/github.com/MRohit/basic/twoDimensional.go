85de2be1943780e1754a05507b714c516d2f5b35
Rohit <mourya.rohit32@gmail.com>
2017-10-12 22:54:37 +0530

    Revising two dimensional arrays

--- src/github.com/MRohit/basic/twoDimensional.go
+++ src/github.com/MRohit/basic/twoDimensional.go
@@ -20,21 +20,39 @@ func main(){
 		}
 		fmt.Printf("\n")
 	}
-	
+	var c[5][2] int;
 	for i:=0; i < 2; i++ {
 		fmt.Printf("\n")
 		for j:=0; j < 2; j++ {
-			a[i][j] = a[i][j] * b[i][j];
+			c[i][j] = a[i][j] * b[i][j];
 		}
-		
+
 	}
-	
-	
+
+
 	fmt.Printf("\nMatrix multiplication result:\n")
 	for i:=0; i < 2; i++ {
 		fmt.Printf("\n")
 		for j:=0; j < 2; j++ {
-			fmt.Printf("%d ",a[i][j])
+			fmt.Printf("%d ",c[i][j])
+		}
+		fmt.Printf("\n")
+	}
+
+	for i:=0; i < 2; i++ {
+		fmt.Printf("\n")
+		for j:=0; j < 2; j++ {
+			c[i][j] = a[i][j] + b[i][j];
+		}
+
+	}
+
+
+	fmt.Printf("\nMatrix Addition result:\n")
+	for i:=0; i < 2; i++ {
+		fmt.Printf("\n")
+		for j:=0; j < 2; j++ {
+			fmt.Printf("%d ",c[i][j])
 		}
 		fmt.Printf("\n")
 	}
